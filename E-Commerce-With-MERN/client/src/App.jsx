20650eac939ba69658722f7759113db4b9986e8a
Piyanshu Pandya <141199362+Piyanshu05@users.noreply.github.com>
2024-10-11 08:27:30 +0530

    Add files via upload

---
+++ E-Commerce-With-MERN/client/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
+import { Toaster } from "react-hot-toast";
+import useUserRoutes from "./components/routes/userRoutes";
+import useAdminRoutes from "./components/routes/adminRoutes";
+
+function App() {
+  const userRoutes = useUserRoutes();
+  const adminRoutes = useAdminRoutes();
+
+  return (
+    <Router>
+      <div className="App">
+        <Toaster position="top-center" />
+        <Header />
+        <div className="container">
+          <Routes>
+            {userRoutes}
+            {adminRoutes}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ E-Commerce-With-MERN/client/src/components/user/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Loader from "../layouts/Loader";
+import PropTypes from "prop-types";
+const ProtectedRoute = ({ admin, children }) => {
+  const { isAuthenticated, user, loading } = useSelector((state) => state.auth);
+
+  if (loading) return <Loader />;
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (admin && user.role !== "admin") {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+ProtectedRoute.propTypes = {
+  children: PropTypes.node,
+};
+
+export default ProtectedRoute;
